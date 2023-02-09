0e5c9d5fa1097217bfd1f6696f8785481aca2ee2
rissmonsuresh <rissmon@gmail.com>
2023-02-09 19:47:48 +0530

    Initial Commit

---
+++ PasswordComponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PasswordComponent
+//
+//  Created by Rissmon Suresh on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
